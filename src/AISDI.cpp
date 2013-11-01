fa5ecb40037a481b8e540c120e8d0e1f3128ba41
Michciu <glibnes@gmail.com>
2013-11-01 20:36:59 +0100

    Initial commit

---
+++ src/AISDI.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : AISDI.cpp
+// Authors     : Michał Dziekoński, Wioletta Klimczak, Szymon Mysiak, Daniel Obrębski, Jakub Skrętowski
+// Copyright   : MIT License (http://opensource.org/licenses/MIT)
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
